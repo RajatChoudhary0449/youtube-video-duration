648a9466da58abdd575ea44015894b26508d262b
Rajat <rishichoudhary08102002@gmail.com>
2025-04-27 18:25:14 +0530

    Worked On Listing of items

--- src/Components/ListItem.jsx
+++ src/Components/ListItem.jsx
@@ -1,10 +1,10 @@
 import React, { useRef, useState } from 'react'
-import formatDuration from '../utils/formatDuration';
 import defaultimage from '../assets/Images/default.jpg'
 import toastNotification from '../utils/toastNotification';
-import { ClipLoader } from 'react-spinners';
 import useTableContext from '../hooks/useTableContext';
-import { isMobile } from '../constant/values';
+import {  isMobile } from '../constant/values';
+import formatDuration from "../utils/formatDuration";
+import { ClipLoader } from "react-spinners";
 import "./../Styles/Transitions/ImageOverlay.css"
 export default function ListItem({ item, openModal }) {
     const offsetlength = isMobile ? 30 : 95;
@@ -41,37 +41,130 @@ export default function ListItem({ item, openModal }) {
             toastNotification('Failed to copy the URL', "error");
         });
     }
-    const ColumnMapping = {
-        "Index": <td key={"Index"} className='align-content-center text-center'>{idx}</td>,
-        "Title": <td key={"Title"} className='align-content-center text-center'><div>{showFull ? (detail.title) : detail.title.slice(0, offsetlength) + "..."}
-            <span className={`${showFull ? "d-none" : "d-inline"} text-primary`} onClick={() => setShowFull(true)} style={{ cursor: 'pointer' }}>Show Complete</span>
-        </div>
-        </td>,
-        "Thumbnail": <td key={"Thumbnail"} className='align-content-center' style={{ cursor: "pointer" }} onClick={() => openModal(detail.id)}>
-            <div className='d-flex justify-content-center'>{isLoading && <div>Loading Image...</div>}
-                <ClipLoader loading={isLoading}></ClipLoader>
-                <div className="imageContainer">
-                    <img src={imageURL} width={width} height={height} alt={`Thumbnail for video titled as: ${detail.title}`} onLoad={handleImageLoad} onError={handleImageError} ref={imgRef} style={{ display: "none" }} className='rounded-3' />
-                    <div className='overlay top-0 rounded-3' style={{width:width,height:height}}>Tap to Play</div>
+    const getColumn = (item, column) => {
+        switch (column) {
+          case "Index":
+            return (
+              <td key={"Index"} className="align-content-center text-center">
+                {idx}
+              </td>
+            );
+          case "Title":
+            return (
+              <td key={"Title"} className="align-content-center text-center">
+                <div>
+                  {showFull
+                    ? detail.title
+                    : detail.title.slice(0, offsetlength) + "..."}
+                  <span
+                    className={`${showFull ? "d-none" : "d-inline"} text-primary`}
+                    onClick={() => setShowFull(true)}
+                    style={{ cursor: "pointer" }}
+                  >
+                    Show Complete
+                  </span>
                 </div>
-            </div>
-        </td>,
-        "Duration": <td key={"Duration"} className='align-content-center text-center'><div>{formatDuration(curtime)}</div></td>,
-        "Published Date": <td key={"Published Date"} className='align-content-center text-center'>{formatDate(detail.publishedAt.slice(0, 10))}</td>,
-        "Action": <td key={"Action"} className='align-content-center text-center' onClick={(e) => { e.stopPropagation() }}>
-            <div className='d-flex flex-row justify-content-center text-center'>
-                <button className='btn border' type='button' title='Preview' onClick={() => openModal(detail.id)}><i className='fas fa-eye'></i></button>
-                <button className='btn border ms-2' type='button' title='Copy' onClick={handleCopy}><i className='fas fa-copy'></i></button>
-            </div>
-        </td>,
-        "Views": <td key={"Views"} className='align-content-center text-center'><div>{item.statistics.viewCount}</div></td>,
-        "Likes": <td key={"Likes"} className='align-content-center text-center'><div>{item.statistics.likeCount}</div></td>,
-        "Comment Count": <td key={"Comment Count"} className='align-content-center text-center'><div>{item.statistics.commentCount}</div></td>,
-    }
+              </td>
+            );
+          case "Thumbnail":
+            return (
+              <td
+                key={"Thumbnail"}
+                className="align-content-center"
+                style={{ cursor: "pointer" }}
+                onClick={() => openModal(detail.id)}
+              >
+                <div className="d-flex justify-content-center">
+                  {isLoading && <div>Loading Image...</div>}
+                  <ClipLoader loading={isLoading}></ClipLoader>
+                  <div className="imageContainer">
+                    <img
+                      src={imageURL}
+                      width={width}
+                      height={height}
+                      alt={`Thumbnail for video titled as: ${detail.title}`}
+                      onLoad={handleImageLoad}
+                      onError={handleImageError}
+                      ref={imgRef}
+                      style={{ display: "none" }}
+                      className="rounded-3"
+                    />
+                    <div
+                      className="overlay top-0 rounded-3"
+                      style={{ width: width, height: height }}
+                    >
+                      Tap to Play
+                    </div>
+                  </div>
+                </div>
+              </td>
+            );
+          case "Duration":
+            return (
+              <td key={"Duration"} className="align-content-center text-center">
+                <div>{formatDuration(curtime)}</div>
+              </td>
+            );
+          case "Published Date":
+            return (
+              <td key={"Published Date"} className="align-content-center text-center">
+                {formatDate(detail.publishedAt.slice(0, 10))}
+              </td>
+            );
+          case "Action":
+            return (
+              <td
+                key={"Action"}
+                className="align-content-center text-center"
+                onClick={(e) => {
+                  e.stopPropagation();
+                }}
+              >
+                <div className="d-flex flex-row justify-content-center text-center">
+                  <button
+                    className="btn border"
+                    type="button"
+                    title="Preview"
+                    onClick={() => openModal(detail.id)}
+                  >
+                    <i className="fas fa-eye"></i>
+                  </button>
+                  <button
+                    className="btn border ms-2"
+                    type="button"
+                    title="Copy"
+                    onClick={handleCopy}
+                  >
+                    <i className="fas fa-copy"></i>
+                  </button>
+                </div>
+              </td>
+            );
+          case "Views":
+            return (
+              <td key={"Views"} className="align-content-center text-center">
+                <div>{item.statistics.viewCount}</div>
+              </td>
+            );
+          case "Likes":
+            return (
+              <td key={"Likes"} className="align-content-center text-center">
+                <div>{item.statistics.likeCount}</div>
+              </td>
+            );
+          case "Comment Count":
+            return (
+              <td key={"Comment Count"} className="align-content-center text-center">
+                <div>{item.statistics.commentCount}</div>
+              </td>
+            );
+        }
+        return <td>Undefined</td>;
+      };
     return (
         <>
             <tr>
-                {column.map((col) => ColumnMapping[col] || null)}
+                {column.map((col) => getColumn(item,col)|| null)}
             </tr>
         </>
     )
--- src/Components/Pages/TableComponent.jsx
+++ src/Components/Pages/TableComponent.jsx
@@ -208,10 +208,10 @@ export default function TableComponent() {
 
     return (
         <>
-            <div className="d-flex justify-content-end align-items-end mt-4">
+            <div className="d-flex justify-content-end align-items-end my-4">
                 <button className="btn btn-secondary" type="button" title="Reset Table" onClick={handleResetClick}><i className="fas fa-arrow-rotate-left"></i></button>
                 {
-                    leftColumn.length>0 && 
+                    leftColumn.length > 0 &&
                     <button
                         className="btn d-flex justify-content-end btn-success text-white gap-1 ms-2 mt-4" style={{ height: "40px" }}
                         type="button"
@@ -228,94 +228,86 @@ export default function TableComponent() {
                         <i className={`fas p-1 fa-caret-${openAddModal ? "up" : "down"}`}></i>
                     </button>
                 }
-                
+
             </div>
-            {leftColumn.length > 0 &&
-                <div className="d-flex pb-2 flex-column">
-                    {openAddModal &&
-                        <div className="d-flex justify-content-end">
-                            <ul className={`list-unstyled ps-2 justify-content-end text-end w-${isMobile?"100":"50"}`}>{leftColumn.map((item, index) => (<li className="p-1 px-3 form-control" key={index} style={{ cursor: "pointer" }} onClick={() => {handleAdd(item); toastNotification(`${item} is successfully added`)}}>{item}</li>))}</ul>
-                        </div>
-                    }
+            {openAddModal &&
+                <div className="d-flex justify-content-end">
+                    <ul className={`list-unstyled ps-2 justify-content-end text-end w-${isMobile ? "100" : "50"}`}>{leftColumn.map((item, index) => (<li className="p-1 px-3 form-control" key={index} style={{ cursor: "pointer" }} onClick={() => { handleAdd(item); toastNotification(`${item} is successfully added`) }}>{item}</li>))}</ul>
                 </div>
             }
-                <ModalWindow
-                    modalOpen={modalOpen}
-                    closeModal={closeModal}
-                    handleOutsideClick={handleOutsideClick}
-                    videoId={videoId}
-                />
-                {column.length === 0 && <p>
-                    No Columns available to display. Add some to begin
-
-                    </p>}
-                <div className="table-wrapper" style={{ position: "relative", maxHeight: "100vh", overflow: "auto" }}>
-                    <table className="table" onClick={() => setOpenAddModal(false)}>
-                        <thead className="position-sticky top-0" style={{zIndex:"1"}}>
-                            <tr>
-                                {column.map((item, index) => (
-                                    <th
-                                        className={"border"}
-                                        id={item}
-                                        draggable
-                                        onDragStart={handleDragStart}
-                                        onDragEnter={handleDragEnter}
-                                        style={{ backgroundColor: "#f4f4f4" }}
-                                        key={index}
+            <ModalWindow
+                modalOpen={modalOpen}
+                closeModal={closeModal}
+                handleOutsideClick={handleOutsideClick}
+                videoId={videoId}
+            />
+            {column.length === 0 && <p>
+                No Columns available to display. Add some to begin
+            </p>}
+            <div className="table-wrapper position-relative" style={{ maxHeight: "100vh", overflow: "auto" }}>
+                <table onClick={() => setOpenAddModal(false)}>
+                    <thead className="position-sticky top-0 z-1" style={{ backgroundColor: "#f4f4f4" }}>
+                        <tr>
+                            {column.map((item, index) => (
+                                <th
+                                    id={item}
+                                    draggable
+                                    onDragStart={handleDragStart}
+                                    onDragEnter={handleDragEnter}
+                                    key={index}
+                                >
+                                    <div
+                                        className={`d-flex flex-${isMobile ? "column" : "row"
+                                            } align-items-center  justify-content-center`}
                                     >
-                                        <div
-                                            className={`d-flex flex-${isMobile ? "column" : "row"
-                                                } align-items-center justify-content-center`}
+                                        <select
+                                            value={item}
+                                            className={`fw-bold bg-white p-2 w-${isMobile?"100":"auto"} border-0 mb-2 mb-md-0`}
+                                            onChange={(e) => handleColumnNameChange(e, index)}
                                         >
-                                            <select
-                                                value={item}
-                                                className={`fw-bold bg-white p-2 w-${isMobile ? "100" : "auto"
-                                                    } border-0`}
-                                                onChange={(e) => handleColumnNameChange(e, index)}
+                                            {totalColumns.map((col, idx) => (
+                                                <option value={col} className="fw-bold" key={idx}>
+                                                    {col}
+                                                </option>
+                                            ))}
+                                        </select>
+                                        <div className="d-flex flex-row">
+                                            <button
+                                                className="btn"
+                                                type={"button"}
+                                                onClick={() => handleSortChange(item)}
                                             >
-                                                {totalColumns.map((col, idx) => (
-                                                    <option value={col} className="fw-bold" key={idx}>
-                                                        {col}
-                                                    </option>
-                                                ))}
-                                            </select>
-                                            <div className="d-flex flex-row">
-                                                <button
-                                                    className="btn"
-                                                    type={"button"}
-                                                    onClick={() => handleSortChange(item)}
-                                                >
-                                                    <i
-                                                        className={`fas fa-sort${sortState[item] === "asc"
-                                                            ? "-up"
-                                                            : sortState[item] === "desc"
-                                                                ? "-down"
-                                                                : ""
-                                                            }`}
-                                                    ></i>
-                                                </button>
-                                                <button
-                                                    className="btn btn-danger ms-1 h-25 p-2 d-flex justify-content-end"
-                                                    type="button"
-                                                    onClick={() => handleDelete(item)}
-                                                >
-                                                    <i className="fas fa-trash"></i>
-                                                </button>
-                                            </div>
+                                                <i
+                                                    className={`fas fa-sort${sortState[item] === "asc"
+                                                        ? "-up"
+                                                        : sortState[item] === "desc"
+                                                            ? "-down"
+                                                            : ""
+                                                        }`}
+                                                ></i>
+                                            </button>
+                                            <button
+                                                className="btn btn-danger ms-1 h-25 p-2 d-flex justify-content-end"
+                                                type="button"
+                                                onClick={() => handleDelete(item)}
+                                            >
+                                                <i className="fas fa-trash"></i>
+                                            </button>
                                         </div>
-                                    </th>
-                                ))}
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {items.map((item) => {
-                                return (
-                                    <ListItem key={item["idx"]} openModal={openModal} item={item} />
-                                );
-                            })}
-                        </tbody>
-                    </table>
-                </div>
+                                    </div>
+                                </th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {items.map((item) => {
+                            return (
+                                <ListItem key={item["idx"]} openModal={openModal} item={item} />
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
         </>
     );
 }
